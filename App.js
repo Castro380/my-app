89a7e9ae0b21fcf8e2a875b6c1610a5816736c1e
Mateus Castro <mateuso@iesb.edu.br>
2023-08-22 22:11:53 -0300

    componentes

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import MeuComponente from './Componentes/MeuComponente';
+import MinMax from './Componentes/MinMax';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <MeuComponente/>
+      <MinMax min="10" max="20" />
       <Text style={[styles.text]}>HELLO WORLD</Text>
       <Image source={require('./assets/favicon.png')} 
       style={{width: 200, height: 200}} />
@@ -14,13 +18,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
 text: {
-  fontSize: 35,
+  fontSize: 25,
   color: 'white'
 }
 });
---
+++ Componentes/MeuComponente.js
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+export default function MeuComponente(){
+    return (
+    <Text>Meu Componente</Text>);
+}
+
+
+
+
+
