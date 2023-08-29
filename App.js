69c90e84380dc3c757e6c7533442267bc6f84275
Castro380 <mateuso@iesb.edu.br>
2023-08-29 17:32:58 -0300

    NumeroAleatorio

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import MeuComponente from './Componentes/MeuComponente';
 import MinMax from './Componentes/semana01/MinMax';
+import NumeroAleatorio from './Componentes/semana02/NumeroAleatorio';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MeuComponente/>
       <MinMax min={10} max={20} />
+      <NumeroAleatorio min={10} max={1000} />
       <Text style={[styles.text]}>HELLO WORLD</Text>
       <Image source={require('./assets/favicon.png')} 
       style={{width: 200, height: 200}} />
--- Componentes/semana02/MeuComponente2.js
+++
@@ -1,9 +0,0 @@
-import { Text } from "react-native"
-
-
-const MeuComponente = function () {
-
-
-}
-
-export default MeuComponente2
